a0d5cd166196b53a2869dad101251c334951de84
anna <a.sahradyan72@gmail.com>
2022-10-19 01:19:19 +0400

    create client directory

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Slider from "./components/Slider";
+import Product from "./pages/Product";
+
+
+const App = () => {
+    return (
+        <>
+          {/*<Slider/>*/}
+            <Product/>
+        </>
+
+    );
+};
+
+export default App;
